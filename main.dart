b2f2cd72eb209d6a4007fd0bdfb5003dcefe8d49
Future1810 <Sfuhr72227@yahoo.de>
2025-02-18 15:35:28 +0100

    Aufgabe1

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main() {
+  String good = "Guten";
+  String day = "Tag";
+  String world = "Welt";
+  int a = 20;
+  String time = "Zeit";
+  int b = 7;
+  int c = 12;
+  String zeichen = "!";
+  int d = 24;
+  String nice = "Schöne";
+  String Morning = "Morgen";
+  String toDay = "Heute";
+  String Free = " ";
+  print("Fertig");
+}
