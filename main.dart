191d08e3bd43e6038cfe3828d653909030e8e1fa
Future1810 <Sfuhr72227@yahoo.de>
2025-03-04 16:05:36 +0100

    Wetter

--- main.dart
+++ main.dart
@@ -1,25 +1,15 @@
 void main() {
-  // Test 1
-  int age = 15;
-  bool hasParentalConsent = true;
-  int movieAgeRating = 16;
+  List<Map<String, double?>> weatherData = [
+    {"temp": 5.3, "rain": 0.9, "wind": null},
+    {"temp": 4.5, "rain": null, "wind": 16.8},
+    {"temp": null, "rain": 3.8, "wind": null},
+  ];
+  List<double?> temps = [];
 
-  if (age >= movieAgeRating ||
-      (hasParentalConsent && (movieAgeRating - age <= 2))) {
-    print("Zugang zum Film erlaubt.");
-  } else {
-    print("Zugang zum Film verweigert.");
-  }
+  temps.add(weatherData[0]["temp"]);
+  temps.add(weatherData[1]["temp"]);
+  temps.add(weatherData[2]["temp"]);
 
-  // Test 2
-  age = 13;
-  hasParentalConsent = false;
-  movieAgeRating = 16;
-
-  if (age >= movieAgeRating ||
-      (hasParentalConsent && (movieAgeRating - age <= 2))) {
-    print("Zugang zum Film erlaubt.");
-  } else {
-    print("Zugang zum Film verweigert.");
-  }
+  double? avgTemp = (temps[0]! + temps[1]! + (temps[2] ?? 0.0) / 3.0);
+  print("durchschnittstemperatur: $avgTemp");
 }
